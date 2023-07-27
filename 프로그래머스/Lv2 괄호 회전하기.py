14c9107517d309c3194d7acdc2c906ff62536b36
zmgu <122958434+zmgu@users.noreply.github.com>
2023-07-27 13:23:01 +0900

    괄호 회전하기

--- 프로그래머스/Lv2 괄호 회전하기.py
+++ 프로그래머스/Lv2 괄호 회전하기.py
@@ -1,1 +1,33 @@
+def check(s):
+    ck = []
+    i = 0
+    while i<len(s):
+        if s[i] in ('(','[','{'):
+            ck.append(s[i])
+        else:
+            if len(ck) == 0:
+                return 0
+            else:
+                if ck[-1] == '(' and s[i] == ')':
+                    ck.pop()
+                elif ck[-1] == '[' and s[i] == ']':
+                    ck.pop()
+                elif ck[-1] == '{' and s[i] == '}':
+                    ck.pop()
+                else:
+                    return 0
+        if len(ck) == 0 and i == len(s)-1:
+            return 1
+        i += 1
 
+def solution(s):
+    answer = 0
+    i = 0
+    if len(s)%2 !=0:
+        return 0
+    while i<len(s):
+        answer += check(s)
+        s = s[1:] + s[0]
+        i +=1
+
+    return answer
