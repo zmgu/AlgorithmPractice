17f757f8266f3bf3d60537c9773c716c1bffc494
JeonMinHyeok <manggu94@gmail.com>
2023-08-02 20:59:57 +0900

    신고 결과 받기

---
+++ 프로그래머스/Lv1 신고 결과 받기.py
@@ -0,0 +1,19 @@
+def solution(id_list, report, k):
+    answer = [0]*len(id_list)
+    
+    report = set(report)
+    report = list(report)
+    arr = [[''] for i in range(len(id_list))]
+    
+    for i in range(0, len(report)):            
+        ids = report[i].split(" ")
+        id_idx = id_list.index(ids[1])
+        arr[id_idx].append(ids[0])
+    
+    for i in range(0, len(arr)):
+        if len(arr[i]) >= k+1:
+            for j in arr[i][1:]:
+                id_idx = id_list.index(j)
+                answer[id_idx] += 1
+
+    return answer
