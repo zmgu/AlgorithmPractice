a472994087c6c78e363d6a58d132114f5af6826b
JeonMinHyeok <manggu94@gmail.com>
2023-08-04 13:25:33 +0900

    행렬 곱셈

--- 프로그래머스/Lv2 행렬 곱셈.py
+++ 프로그래머스/Lv2 행렬 곱셈.py
@@ -4,7 +4,8 @@ def solution(arr1, arr2):
     for arr1_row in range(0, len(arr1)):
         for arr2_col in range(0, len(arr2[0])):
             hap = 0
-            for arr2_row in range(0, len(arr2)):                
+            for arr2_row in range(0, len(arr2)):
+                
                 hap += arr1[arr1_row][arr2_row] * arr2[arr2_row][arr2_col]
             answer[arr1_row][arr2_col] = hap
                         
