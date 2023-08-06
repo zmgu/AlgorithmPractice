88654691b5dbc17937955e6e0ba6a0cda5a0553e
JeonMinHyeok <manggu94@gmail.com>
2023-08-06 18:52:33 +0900

    [1차] 뉴스 클러스터링

--- 프로그래머스/Lv2 [1차] 뉴스 클러스터링.py
+++ 프로그래머스/Lv2 [1차] 뉴스 클러스터링.py
@@ -1,25 +1,25 @@
 def solution(str1, str2):
     answer = 0
-    ck_arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
     str1, str2 = str1.lower() , str2.lower()
 
     arr1 = []
     arr2 = []
     for i in range(1,len(str1)):
-        if str1[i-1] in ck_arr and str1[i] in ck_arr:
+        if str1[i-1].isalpha() == True and str1[i].isalpha() == True:
             if i == len(str1)-1:
                 ck = str1[i-1:]
             else:
                 ck = str1[i-1:i+1]
             arr1.append(ck)
     for i in range(1,len(str2)):
-        if str2[i-1] in ck_arr and str2[i] in ck_arr:
+        if str2[i-1].isalpha() == True and str2[i].isalpha() == True:
             if i == len(str2)-1:
                 ck = str2[i-1:]
             else:
                 ck = str2[i-1:i+1]
             arr2.append(ck)
-
+    
     if len(arr2) >= len(arr1):
         temp = arr1
         arr1 = arr2
@@ -36,7 +36,6 @@ def solution(str1, str2):
             arr2.remove(arr2[i])
         else:
             i += 1
-    
     
     uni = lenarr1 + len(arr2)
     if hap == 0:
